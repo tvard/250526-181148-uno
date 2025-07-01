38b5daa15ec93bbcb0d08b0983d856055ddfc79a
Theo Vardakis <theodosiovardakis@gmail.com>
2025-07-01 03:17:16 +0300

    Major Refactoring. Added unit tests. Fixes based on test cases

--- lib/helpers/helpers.h
+++ lib/helpers/helpers.h
@@ -18,6 +18,12 @@ const int SCAN_INTERVAL    = 300; // Time between distance measurements
 const int LOOP_DELAY_MS = 1; // how often we run the main loop
 const int RAMP_STEP = 30;    // how many speed units we change per loop, less = smoother but slower response
 
+// 2) Compute target speeds in some basic scenarios => FWD/BWD, L/R (sharp turn)
+//    constaint to MIN_MOTOR_SPEED, maxMotorSpeed and apply ramping
+const int JOYSTICK_DEADZONE = 18;                      // deadzone around center based on joystick variance
+const int FORWARD_THRESHOLD = 512 + JOYSTICK_DEADZONE;  // center = 512, forward = 1023  (center can change with usage)
+const int BACKWARD_THRESHOLD = 512 - JOYSTICK_DEADZONE; // center = 512, backward = 0
+
 // function declarations
 int slewRateLimit(int current, int target);
 
@@ -29,3 +35,51 @@ String pad5f(float val);
 std::string pad5(int val);
 std::string pad5f(float val);
 #endif
+
+
+struct JoystickInput {
+    int x;
+    int y;
+};
+
+struct JoystickProcessingResult {
+    float rawRatioLR;
+    float steppedRatioLR;
+    float quantizeStep;
+    int correctedX;
+    int correctedY;
+    bool buzzerOn;
+};
+
+struct MotorTargets {
+    int left;
+    int right;
+    bool skipSlewRate;
+};
+
+struct ManualModeInputs {
+    JoystickProcessingResult joystick;
+    int leftSpeed;
+    int rightSpeed;
+    int prevLeftSpeed;
+    int prevRightSpeed;
+    // ...add more as needed
+};
+
+struct ManualModeOutputs {
+    int leftSpeed;
+    int rightSpeed;
+    int outputLeft;
+    int outputRight;
+    bool brakingApplied;
+    bool skipSlewRate;
+    bool buzzerOn;
+    // ...add more as needed
+};
+
+JoystickProcessingResult processJoystick(int joystickX, int joystickY, bool joystickButton = false);
+MotorTargets computeMotorTargets(const JoystickProcessingResult& js, int prevLeft, int prevRight);
+long map(float steppedRatioLR);
+bool shouldSkipSlewRate(int prevLeft, int prevRight, int targetLeft, int targetRight);
+bool shouldApplyBraking(int prevLeft, int prevRight, int targetLeft, int targetRight);
+ManualModeOutputs manualModeStep(const ManualModeInputs& in);
